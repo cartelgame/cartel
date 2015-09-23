1878c6421d7d3a8988ac80bf0009e7243f005f75
deadpassive <mankillseverything@gmail.com>
2015-09-23 13:21:50 +0100

    First proper commit

---
+++ index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.use(express.static('public'));
+
+io.on('connection', function(socket){
+  console.log(socket);
+  console.log('a user connected');
+  io.emit('connection message', "User connected");
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+    io.emit('connection message', "User disconnected");
+  });
+  
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, '0.0.0.0', function(){
+  console.log('listening on *:3000');
+});
