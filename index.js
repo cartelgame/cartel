165e11f0f857f8ec5e79d6fe31ce4eeba51485c9
deadpassive <mankillseverything@gmail.com>
2015-09-28 11:23:58 +0100

    Started adding mongo stuff

--- index.js
+++ index.js
@@ -1,36 +1,53 @@
-var express = require('express');
+var express = require('express'),
+	http = require('http').Server(app),
+	io = require('socket.io')(http),
+	session = require('express-session');
 
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/pages/index.html');
+var Session = require('express-session'),
+    SessionStore = require('session-file-store')(Session);
+    session = Session({
+      store: new SessionStore({ path: './tmp/sessions' }),
+      secret: 'pass',
+      resave: true,
+      saveUninitialized: true
+    });
+
+app.use(session({
+	secret:'pass',
+	resave: true,
+	saveUninitialized: true
+}));
+
+io.use(function(socket, next) {
+	session(socket.handshake, {}, next);
 });
 
-app.get('/test', function(req, res){
-  res.sendFile(__dirname + '/pages/test.html');
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/pages/index.html');
 });
 
 app.use(express.static('public'));
 app.use(express.static('bower_components'));
 
 io.on('connection', function(socket){
-  console.log(socket);
-  console.log('a user connected');
-  io.emit('connection message', "User connected");
+	// console.log("Session: ", socket.handshake.session);
+	// console.log(socket);
+	console.log('a user connected');
+	io.emit('connection message', "User connected");
 
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-    io.emit('connection message', "User disconnected");
-  });
-  
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg.user + " - " + msg.message);
-    io.emit('chat message', msg);
-  });
+	socket.on('disconnect', function(){
+		console.log('user disconnected');
+		io.emit('connection message', "User disconnected");
+	});
+	
+	socket.on('chat message', function(msg){
+		console.log('message: ' + socket.handshake.session.uid + " - " + msg.message);
+		io.emit('chat message', msg);
+	});
 });
 
-http.listen(3000, '0.0.0.0', function(){
-  console.log('listening on *:3000');
+app.listen(3000, '0.0.0.0', function(){
+	console.log('listening on *:3000');
 });
