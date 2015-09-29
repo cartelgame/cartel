e34ae6b99eb22cd71bfadbd9ea8d065353b17eaa
deadpassive <mankillseverything@gmail.com>
2015-09-29 17:11:40 +0100

    Started adding authentication

--- index.js
+++ index.js
@@ -1,39 +1,110 @@
-var express = require('express'),
-	http = require('http').Server(app),
-	io = require('socket.io')(http),
-	session = require('express-session');
-
+var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var mongoose = require('mongoose');
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var hash = require('./pass').hash;
 
-var Session = require('express-session'),
-    SessionStore = require('session-file-store')(Session);
-    session = Session({
-      store: new SessionStore({ path: './tmp/sessions' }),
-      secret: 'pass',
-      resave: true,
-      saveUninitialized: true
-    });
+var configDB = require('./config/database.js');
 
+mongoose.connect(configDB.url); // connect to our database
+
+// set up our express application
+// app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.urlencoded());
 app.use(session({
-	secret:'pass',
-	resave: true,
-	saveUninitialized: true
+	secret: 'secret',
+	key: 'express.sid',
+	saveUninitialized: true,
+	resave: true
 }));
-
-io.use(function(socket, next) {
-	session(socket.handshake, {}, next);
-});
-
-app.get('/', function(req, res){
-	res.sendFile(__dirname + '/pages/index.html');
-});
 
 app.use(express.static('public'));
 app.use(express.static('bower_components'));
 
+// dummy database
+
+var users = {
+	tj: { name: 'tj' }
+};
+
+function restrict(req, res, next) {
+	if (req.session.user) {
+		next();
+	} else {
+		req.session.error = 'Access denied!';
+		req.session.returnTo = req.path;
+		res.redirect('/login');
+	}
+}
+
+// when you create a user, generate a salt
+// and hash the password ('foobar' is the pass here)
+
+hash('foobar', function(err, salt, hash){
+	if (err) throw err;
+	// store the salt & hash in the "db"
+	users.tj.salt = salt;
+	users.tj.hash = hash.toString();
+});
+
+function authenticate(name, pass, fn) {
+	if (!module.parent) console.log('authenticating %s:%s', name, pass);
+	var user = users[name];
+	// query the db for the given username
+	if (!user) return fn(new Error('cannot find user'));
+	// apply the same algorithm to the POSTed password, applying
+	// the hash against the pass / salt, if there is a match we
+	// found the user
+	hash(pass, user.salt, function(err, hash){
+		if (err) return fn(err);
+		if (hash.toString() == user.hash) return fn(null, user);
+		fn(new Error('invalid password'));
+	})
+}
+
+app.get('/', restrict, function(req, res){
+  	res.sendfile('pages/index.html');
+});
+
+app.get('/login', function(req, res){
+  	res.sendfile('pages/login.html');
+});
+
+app.post('/login', function(req, res){
+	authenticate(req.body.username, req.body.password, function(err, user){
+		if (user) {
+			// Regenerate session when signing in
+			// to prevent fixation
+			req.session.regenerate(function(){
+				// Store the user's primary key
+				// in the session store to be retrieved,
+				// or in this case the entire user object
+				req.session.user = user;
+				req.session.success = 'Authenticated as ' + user.name
+					+ ' click to <a href="/logout">logout</a>. '
+					+ ' You may now access <a href="/restricted">/restricted</a>.';
+				var returnTo = req.session.returnTo ? req.session.returnTo : '/';
+				delete req.session.returnTo;
+				//is authenticated ?
+				res.redirect(returnTo);
+			});
+		} else {
+			req.session.error = 'Authentication failed, please check your '
+				+ ' username and password.'
+				+ ' (use "tj" and "foobar")';
+			res.redirect('login');
+		}
+	});
+});
+
+
 io.on('connection', function(socket){
-	// console.log("Session: ", socket.handshake.session);
-	// console.log(socket);
 	console.log('a user connected');
 	io.emit('connection message', "User connected");
 
@@ -41,13 +112,13 @@ io.on('connection', function(socket){
 		console.log('user disconnected');
 		io.emit('connection message', "User disconnected");
 	});
-	
+
 	socket.on('chat message', function(msg){
-		console.log('message: ' + socket.handshake.session.uid + " - " + msg.message);
+		console.log('message: ' + msg.user + " - " + msg.message);
 		io.emit('chat message', msg);
 	});
 });
 
-app.listen(3000, '0.0.0.0', function(){
-	console.log('listening on *:3000');
+http.listen(3000, function(){
+  	console.log('listening on *:3000');
 });
