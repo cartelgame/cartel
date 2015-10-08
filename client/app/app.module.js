8923e1d393de7eaf28592950b45eda05b983eee2
deadpassive <mankillseverything@gmail.com>
2015-10-08 15:20:11 +0100

    More app layout changes

--- client/app/app.module.js
+++ client/app/app.module.js
@@ -2,24 +2,24 @@
 	angular.module('cartel', ['ngRoute', 'ngCookies', 'btford.socket-io', 'base64', 'ngStorage'])
 		.config(['$routeProvider', '$httpProvider',
 			function($routeProvider, $httpProvider) {
-				$routeProvider.
-					when('/login', {
-						templateUrl: 'partials/views/login.html',
+				$routeProvider
+					.when('/login', {
+						templateUrl: 'app/auth/login.view.html',
 						controller: 'LoginController'
-					}).
-					when('/register', {
-						templateUrl: 'partials/views/register.html',
+					})
+					.when('/register', {
+						templateUrl: 'app/auth/register.view.html',
 						controller: 'RegisterController'
-					}).
-					when('/games', {
+					})
+					.when('/games', {
 						templateUrl: 'partials/views/games.html',
 						controller: 'GameListController'
-					}).
-					when('/games/:gameId', {
-						templateUrl: 'partials/views/game.html',
+					})
+					.when('/games/:gameId', {
+						templateUrl: 'app/game/game.view.html',
 						controller: 'GameController'
-					}).
-					otherwise({
+					})
+					.otherwise({
 			        	redirectTo: '/login'
 			      	});
 			    $httpProvider.interceptors.push('TokenInterceptorService');
