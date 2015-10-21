7607de4f9f74912987cbb72528fb0ead67b249ae
deadpassive <mankillseverything@gmail.com>
2015-10-21 14:25:49 +0100

    PlayerState now creates methods on prototype for consistency

--- app/game/player-state.js
+++ app/game/player-state.js
@@ -2,23 +2,23 @@ function PlayerState(player, position) {
 	this.player = player;
 	this.position = position;
 	this.ownedTiles = [];
+}
 
-	this.add = function(tile) {
-		this.ownedTiles.push(tile);
-	};
+PlayerState.prototype.add = function(tile) {
+	this.ownedTiles.push(tile);
+};
 
-	this.remove = function(tile) {
-		var index = -1;
-		for (var i=0;i<this.ownedTiles.length;i++) {
-			if (tile===this.ownedTiles[i]) {
-				index = i;
-				break;
-			}
+PlayerState.prototype.remove = function(tile) {
+	var index = -1;
+	for (var i=0;i<this.ownedTiles.length;i++) {
+		if (tile===this.ownedTiles[i]) {
+			index = i;
+			break;
 		}
-		if (index > -1) {
-			delete this.ownedTiles[index];
-		}
-	};
+	}
+	if (index > -1) {
+		delete this.ownedTiles[index];
+	}
 }
 
 module.exports = PlayerState;
