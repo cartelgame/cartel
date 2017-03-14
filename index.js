4660e32bbfc160321fe2418fa3a1a019ea21936c
Mikey Robins <mike.robins@misoportal.com>
2017-03-14 11:59:20 +0000

    Call default tileset reset
    missed from the other commit. calls the internal API

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var callhttp = require('http');
 var mongoose = require('mongoose');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
@@ -13,83 +14,6 @@ var TileSet = require('./app/models/tileset')
 var configDB = require('./config/database.js');
 
 mongoose.connect(configDB.url);
-
-// Setup defaut tileset
-async.waterfall([
-	function(callback) {
-		TileSet.findOne({name: 'default'}, function(err, tileset) {
-			if (err) {
-				throw err;
-			}
-			if (tileset) {
-				console.log("Default tileset already exists");
-				return callback(null, false);
-			} else {
-				TileSet.remove({}, function() {
-					console.log("Cleared tilesets");
-					callback(null, true);
-				});
-			}
-		})
-	},
-	function(addTileset, callback) {
-		if (!addTileset) {
-			return callback();
-		} else {
-			var tileset = new TileSet({
-				name: "default",
-				tiles: [
-					{name: 'Go!', 				visitingValue: 200, purchasable: false},
-					{name: 'Minish Woods', 		purchasable: true, cost: 60, 	group: 'a'},
-					{name: 'Ordon Village', 	purchasable: true, cost: 60, 	group: 'a'},
-					{name: 'Kokiki Forest', 	purchasable: true, cost: 60, 	group: 'b'},
-					{name: 'Lost Woods', 		purchasable: true, cost: 100, 	group: 'b'},
-					{name: 'Forest Temple', 	purchasable: true, cost: 100, 	group: 'b'},
-					{name: 'Graveyard', 		purchasable: true, cost: 200, 	group: 'c'},
-					{name: 'Kakariko Village', 	purchasable: true, cost: 100, 	group: 'c'},
-					{name: 'Shadow Temple', 	purchasable: true, cost: 200, 	group: 'c'},
-					{name: 'Haunted Wasteland', purchasable: true, cost: 200, 	group: 'd'},
-					{name: 'Gerudo Fortress', 	purchasable: true, cost: 200, 	group: 'd'},
-					{name: 'Spirit Template', 	purchasable: true, cost: 200, 	group: 'd'},
-					{name: 'Death Mountain', 	purchasable: true, cost: 200, 	group: 'e'},
-					{name: 'Goron City', 		purchasable: true, cost: 100, 	group: 'e'},
-					{name: 'Fire Temple', 		purchasable: true, cost: 200, 	group: 'e'},
-					{name: 'Lon Lon Ranch', 	purchasable: true, cost: 200, 	group: 'f'},
-					{name: 'Hyrule Castle', 	purchasable: true, cost: 200, 	group: 'f'},
-					{name: 'Temple of Time', 	purchasable: true, cost: 100, 	group: 'f'},
-					{name: 'Lake Hylia', 		purchasable: true, cost: 200, 	group: 'g'},
-					{name: 'Zora\'s Domain', 	purchasable: true, cost: 200, 	group: 'g'},
-					{name: 'Water Temple', 		purchasable: true, cost: 200, 	group: 'g'},
-					{name: 'Skyloft', 			purchasable: true, cost: 200, 	group: 'h'},
-					{name: 'City in the Sky', 	purchasable: true, cost: 200, 	group: 'h'},
-					{name: 'w', purchasable: true, cost: 200, group: 'f'},
-					{name: 'x', purchasable: true, cost: 200, group: 'f'},
-					{name: 'y', purchasable: true, cost: 200, group: 'f'},
-					{name: 'z', purchasable: true, cost: 200, group: 'f'},
-					{name: 'aa', purchasable: true, cost: 200, group: 'f'},
-					{name: 'ab', purchasable: true, cost: 200, group: 'f'},
-					{name: 'ac', purchasable: true, cost: 200, group: 'f'},
-					{name: 'ad', purchasable: true, cost: 200, group: 'f'},
-					{name: 'ae', purchasable: true, cost: 200, group: 'f'},
-					{name: 'af', purchasable: true, cost: 200, group: 'f'},
-					{name: 'ag', purchasable: true, cost: 200, group: 'f'},
-					{name: 'ah', purchasable: true, cost: 200, group: 'f'},
-					{name: 'ai', purchasable: true, cost: 200, group: 'f'},
-					{name: 'aj', purchasable: true, cost: 200, group: 'f'},
-					{name: 'ak', purchasable: true, cost: 200, group: 'f'},
-					{name: 'al', purchasable: true, cost: 200, group: 'f'},
-					{name: 'am', purchasable: true, cost: 200, group: 'f'},
-					{name: 'an', purchasable: true, cost: 200, group: 'f'}
-				]
-			});
-
-			tileset.save(function() {
-				console.log("Saved tileset " + tileset);
-				callback();
-			});
-		}
-	}
-]);
 
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
@@ -110,6 +34,8 @@ app.use(express.static('client'));
 app.use('/', require('./app/routes.js'))
 app.use('/api', require('./app/apiRoutes.js'))
 
+callDefaultTileSet();
+
 // passport config
 var User = require('./app/models/user');
 passport.use(new LocalStrategy(User.authenticate()));
@@ -122,3 +48,25 @@ require('./app/sockets')(http);
 http.listen(3000, function(){
   	console.log('listening on *:3000');
 });
+
+function callDefaultTileSet(){
+	TileSet.findOne({name: 'default'}, function(err, tileset) {
+			if (err) {
+				throw err;
+			}
+			if (tileset) {
+				console.log("Default tileset already exists");
+				return true;
+			} else {
+				var options = {
+				  host: "",
+				  port: 3000,
+				  path: '/api/resetTileSet',
+				  method: 'GET'
+				};
+				callhttp.request(options, function(res) {
+					console.log("made Request");				  
+				}).end();
+			}
+		});
+}
